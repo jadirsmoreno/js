a53de54c7c295273a0c567f64d0a7b5c5a07244b
Jadir Moreno <jadirsmoreno@gmail.com>
2022-03-21 18:13:40 -0300

    Confirmar

---
+++ arrays-listaDuasDimensões.js
@@ -0,0 +1,13 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana']
+
+const mediasDosAlunos = [10, 7, 9, 6]
+
+let listaDeNotasEAlunos = [alunos, mediasDosAlunos]
+
+
+// [0] [0] primeiramente a array listaDeNotasEAlunos acessa como índice 0 a lista
+// [const alunos] e dentro da [const alunos] acessa o índice zero que é o nome João
+
+//[1] [0] acessa a lista de índice 1 [mediasDosAlunos], dentro desta lista 
+// acessa a primeira nota [10]
+console.log(`${listaDeNotasEAlunos[0][0]}, sua média é ${listaDeNotasEAlunos[1][0]}`)
